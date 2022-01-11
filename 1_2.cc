f292d501c3be77dc03e79c638505062741286e14
Akrat Chandola <akratchandola@gmail.com>
2022-01-11 18:02:51 +0530

    Add files via upload

---
+++ 1_2.cc
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define N 5
+using namespace std;
+int main()
+{
+	int i,j;
+	for(i=0;i<N;i++)
+	{
+		for(j=N-1;j>i;j--)
+			cout<<" ";
+		for(j=0;j<=i;j++)
+			cout<<"*";
+		cout<<endl;
+	}
+}
